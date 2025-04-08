7532977340c320ebcf74b7492b509ae0b0e18660
Almudena <202204789@alu.comillas.edu>
2025-04-08 16:54:34 +0200

    Cambios CaptionDataset (data.py) agilizar

--- src/data.py
+++ src/data.py
@@ -47,7 +47,7 @@ class ImageDataset(Dataset):
     
 
 class CaptionDataset(Dataset):
-    def __init__(self, image_data, model, processor, device, caption_file=None):
+    def __init__(self, dataloader, caption_file=None):
         """
         Args:
             image_data (list): List of images with their names.
@@ -56,24 +56,33 @@ class CaptionDataset(Dataset):
             device: The device ('cpu' or 'cuda').
             caption_file: Path to CSV with pre-existing captions.
         """
+        
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        checkpoint = "microsoft/git-base"
+        processor = AutoProcessor.from_pretrained(checkpoint)
+        model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+        
         self.image_names = []
         self.captions = []
         self.device = device
 
         # Check if CSV file with captions already exists
         if caption_file and os.path.exists(caption_file):
-            df = pd.read_csv(caption_file)
+            df = pd.read_csv(caption_file, sep=";")
             self.image_names = df['image_name'].tolist()
             self.captions = df['caption'].tolist()
+        
         else:
             try:
-                for img_name, image in tqdm(image_data, desc="Generating captions"):
-                    inputs = processor(images=image, return_tensors="pt").to(device)
-                    with torch.no_grad():
-                        generated_ids = model.generate(pixel_values=inputs.pixel_values, max_length=50)
-                        caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-                        self.image_names.append(img_name)
-                        self.captions.append(caption)
+                for names, images in tqdm(dataloader, desc="Generating captions"):
+                    images = images.to(self.device)
+                    for name, image in zip(names, images):
+                        inputs = self.processor(images=image, return_tensors="pt").to(self.device)
+                        with torch.no_grad():
+                            generated_ids = self.model.generate(pixel_values=inputs.pixel_values, max_length=50)
+                            caption = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+                            self.image_names.append(name)
+                            self.captions.append(caption)
 
                 # Save captions to CSV if they were generated
                 if caption_file:
@@ -94,27 +103,9 @@ class CaptionDataset(Dataset):
         img_name = self.image_names[idx]
         caption = self.captions[idx]
         return img_name, caption
-    
-
-def load_image_data(root_dirs, batch_size=32, num_workers=0):
-    """
-    Loads the images from the specified directories into a DataLoader.
-
-    Args:
-        root_dirs (list): List of directories to search for images.
-        batch_size (int): The batch size for the DataLoader.
-        num_workers (int): Number of workers to use for data loading.
-
-    Returns:
-        DataLoader: DataLoader for the images.
-    """
-    dataset = ImageDataset(root_dirs)
-    
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    return dataloader
 
 
-def initialize_datasets(batch_size=32, num_workers=0, caption_file='captionsImages.csv'):
+def initialize_datasets(batch_size=32, num_workers=0):
     """
     Initializes the image and caption datasets and returns both.
 
@@ -127,22 +118,13 @@ def initialize_datasets(batch_size=32, num_workers=0, caption_file='captionsImag
         image_dataset (ImageDataset): Dataset containing the images.
         caption_dataset (CaptionDataset): Dataset containing the generated captions.
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    checkpoint = "microsoft/git-base"
-    processor = AutoProcessor.from_pretrained(checkpoint)
-    model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)  # Add trust_remote_code=True
     root_dirs = ["data/twitter2015_images/", "data/twitter2017_images/"]
-
-    dataloader = load_image_data(root_dirs, batch_size=batch_size, num_workers=num_workers)
-
-    image_data = []
-    for names, images in tqdm(dataloader, desc="Preparing images"):
-        images = images.to(device)
-        for name, image in zip(names, images):
-            image_data.append((name, image))
-
-    caption_dataset = CaptionDataset(image_data, model, processor, device, caption_file)
+    caption_file = 'captionsImages.csv'
+                
     image_dataset = ImageDataset(root_dirs)
+    dataloader = DataLoader(image_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    
+    caption_dataset = CaptionDataset(dataloader, caption_file)
 
     return image_dataset, caption_dataset
 
